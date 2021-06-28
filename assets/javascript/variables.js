491f8fd4335933e836234bc91155c03c21f75a5b
Carter <ASHLEY.CARTER@LibertyMutual.com>
2021-06-28 12:09:53 -0400

    variables section complete

--- assets/javascript/variables.js
+++ assets/javascript/variables.js
@@ -1,2 +1,14 @@
 console.log("variables.js")
 
+let total = 150;
+const product = 'monitor';
+var discounted = true;
+
+total = 200;
+discounted = false;
+product = 'monitor v.2';
+
+console.log(total);
+console.log(product);
+console.log(discounted);
+
