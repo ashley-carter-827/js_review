d2f3d8a215dc055f8fbfaa4c12a761a1f1ba9f4c
Carter <ASHLEY.CARTER@LibertyMutual.com>
2021-06-28 14:23:03 -0400

    numbers section complete

---
+++ assets/javascript/numbers.js
@@ -0,0 +1,18 @@
+console.log("numbers.js")
+
+let num = 22;
+console.log(num);
+
+num = num + 15;
+console.log(num);
+
+num -= 8;
+console.log(num);
+
+let average = (26+44+72+86+112)/5
+console.log(average);
+
+let theNumber = 14; 
+console.log(++theNumber);
+
+console.log(typeof num);
